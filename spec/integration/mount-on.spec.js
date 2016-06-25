001d6a9419b518ce48c65c9146ffd752564df9a5
Ægir Örn Símonarson <agirorn@gmail.com>
2016-06-25 00:06:09 +0000

    Control integration test with JASMINE_CONFIG_PATH

--- spec/integration/mount-on.spec.js
+++ spec/integration/mount-on.spec.js
@@ -4,46 +4,44 @@ var exec = require('child_process').exec;
 var Vagrant = require('../helpers/vagrant');
 var vagrant = new Vagrant();
 
-if (process.env.NODE_ENV === 'integration') {
-  describe('mount-on', function () {
-    var timeout = 1000000;
-    var local = new Vagrant('local');
+describe('mount-on', function () {
+  var timeout = 1000000;
+  var local = new Vagrant('local');
 
-    beforeAll(function (done) {
-      fs.mkdirsSync('.tmp/local-folder');
-      fs.mkdirsSync('.tmp/remote-folder');
-      touch.sync('.tmp/local-folder/new-file');
-      vagrant.up(done);
+  beforeAll(function (done) {
+    fs.mkdirsSync('.tmp/local-folder');
+    fs.mkdirsSync('.tmp/remote-folder');
+    touch.sync('.tmp/local-folder/new-file');
+    vagrant.up(done);
+  }, timeout);
+
+  afterEach(function () {
+    fs.removeSync('.tmp/local-folder');
+    fs.removeSync('.tmp/remote-folder');
+  });
+
+  describe('mount', function () {
+    beforeEach(function (done) {
+      local.sshCommand([
+        'export NVM_DIR="/home/vagrant/.nvm";',
+        '. "$NVM_DIR/nvm.sh";', // # This loads nvm
+        '/vagrant/bin/mount-on.js',
+        '--local-host=192.168.33.20',
+        '--local-folder=/vagrant/.tmp/local-folder',
+        '--local-user=vagrant',
+        '--local-password=vagrant',
+        '--remote-host=192.168.33.10',
+        '--remote-folder=/vagrant/.tmp/remote-folder',
+        '--remote-user=vagrant',
+        '--remote-password=vagrant',
+        '--autoconnect'
+      ].join(' '), done);
     }, timeout);
 
-    afterEach(function () {
-      fs.removeSync('.tmp/local-folder');
-      fs.removeSync('.tmp/remote-folder');
-    });
-
-    describe('mount', function () {
-      beforeEach(function (done) {
-        local.sshCommand([
-          'export NVM_DIR="/home/vagrant/.nvm";',
-          '. "$NVM_DIR/nvm.sh";', // # This loads nvm
-          '/vagrant/bin/mount-on.js',
-          '--local-host=192.168.33.20',
-          '--local-folder=/vagrant/.tmp/local-folder',
-          '--local-user=vagrant',
-          '--local-password=vagrant',
-          '--remote-host=192.168.33.10',
-          '--remote-folder=/vagrant/.tmp/remote-folder',
-          '--remote-user=vagrant',
-          '--remote-password=vagrant',
-          '--autoconnect'
-        ].join(' '), done);
-      }, timeout);
-
-      it('works', function () {
-        exec("vagrant ssh remote -c 'file /vagrant/.tmp/remote-folder/new-file'", function (err, stdout, stderr) {
-          expect(err).toBe(null);
-        });
+    it('works', function () {
+      exec("vagrant ssh remote -c 'file /vagrant/.tmp/remote-folder/new-file'", function (err, stdout, stderr) {
+        expect(err).toBe(null);
       });
     });
   });
-}
+});
